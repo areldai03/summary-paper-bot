06ba2f2a5019cd8c521ff47c47e739cc06b4a670
areldai03 <dai.0127@icloud.com>
2025-09-26 14:10:35 +0900

    fix: .gitignoreにscriptts/を追加し、run.shの実行環境を更新

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=arxiv_nlp_bot
 #SBATCH --output=logs/slurm-%x-%j.out
 #SBATCH --time=00:30:00           # 最大実行時間
-#SBATCH --partition=hestia       # 利用するパーティション名
-#SBATCH --gres=gpu:1              # GPU が必要なら指定
+#SBATCH --partition=varuna       # 利用するパーティション名
+#SBATCH --gres=gpu:4              # GPU が必要なら指定
 #SBATCH --cpus-per-task=8         # CPU コア数
-#SBATCH --mem=16G                 # メモリ
+#SBATCH --mem=48G                 # メモリ
 
-uv run python3 src/main.py
+uv run python3 /home/maekawa/summary-paper-bot/src/main.py
